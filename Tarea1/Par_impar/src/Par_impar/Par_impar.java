a0a1c1d580544548629501d50a197713f2f58fc8
Lenocarft <programandolenocraft@gmail.com>
2026-05-31 21:06:03 -0400

    Update Par_impar.java

--- Tarea1/Par_impar/src/Par_impar/Par_impar.java
+++ Tarea1/Par_impar/src/Par_impar/Par_impar.java
@@ -14,6 +14,7 @@ public class Par_impar {
         
         if(numero % 2 == 0){
             System.out.println("Es par");
+            System.out.println("Es no par");
         }
         else{
             System.out.println("Es impar");
